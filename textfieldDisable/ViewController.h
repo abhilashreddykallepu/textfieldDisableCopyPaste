54b46bb20473c406dde4f056203d2efa5797b389
Abhilash Reddy Kallepu <HadiHatinoglu@Hadis-iMac.local>
2013-12-04 18:08:27 +0530

    Initial Commit

---
+++ textfieldDisable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  textfieldDisable
+//
+//  Created by Hadi Hatinoglu on 04/12/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
